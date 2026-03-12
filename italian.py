64a0c027d2feec9fd2b9dde3f592a0b1ab172534
bmastroviti <bmastroviti@stsaviour.org>
2026-03-12 11:47:33 -0400

    finish

--- italian.py
+++ italian.py
@@ -7,6 +7,10 @@ if __name__ == '__main__':
         'What is the capital of Italy?': ['Rome', 'Venice', 'Florence', 'Milan'],
         'How do you say "Hello" in Italian?': ['Ciao', 'arrivederci', 'buongiorno', 'buonasera'],
     'What is the most common food eaten in Italy?':['Pizza', 'Pasta', 'Hamburger', 'Salad'], 
+    'What is the most common dessert eaten in Italy?': ['Tiramisu', 'cannoli', 'gelato', 'Affagoto'], 
+    'What landmark  is in Italy?': ['Leaning Tower of Pisa', 'Eiffel Tower', 'Mount Fuji', 'Forbidden City'],
+    'What is the smallest type of pasta?': ['Pastina', 'orzo', 'Ditalini', 'stelline'], 
+    'What is the largest type of pasta?': ['Caccavelle', ]
     }
     
 
